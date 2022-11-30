87b0bcd63eee791c1e90072f982b37677cc01820
MandeepSingh6 <101950170+MandeepSingh6@users.noreply.github.com>
2022-11-30 01:59:27 -0500

    Update AddImagesPage.js

--- client/src/pages/AddImagesPage.js
+++ client/src/pages/AddImagesPage.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const AddImagesPage = () => {
   const navigate = useNavigate();
   // const url = "http://localhost:4000/images";
-  const url = "https://animalkingdompark.onrender.com";
+  const url = "https://animalkingdomparkbackend.onrender.com";
   const [uploadedImage, setUploadedImage] = useState("");
   const [images, setImages] = useState({
     name: "",
